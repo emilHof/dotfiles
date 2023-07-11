6b18a6154e5e6ee6bcf303f9baa9b155e762488f
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-07-11 01:09:55 -0400

    Feat(nvim): Add python to treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,31 +1,32 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "diff" },
-  sync_install = false,
-  auto_install = true,
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "diff", "norg",
+		"norg_meta", "python" },
+	sync_install = false,
+	auto_install = true,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  }
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = 'o',
+			toggle_hl_groups = 'i',
+			toggle_injected_languages = 't',
+			toggle_anonymous_nodes = 'a',
+			toggle_language_display = 'I',
+			focus_language = 'f',
+			unfocus_language = 'F',
+			update = 'R',
+			goto_node = '<cr>',
+			show_help = '?',
+		},
+	}
 }
 
 vim.keymap.set("n", "<leader>tsp", vim.cmd.TSPlaygroundToggle)
