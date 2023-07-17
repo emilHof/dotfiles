f43344f01c5e5c8ace57928a09b8cad3115d5a24
Emil Hofstetter <emilhof@amazon.com>
2023-07-17 14:51:17 +0000

    Feat(nvim): Rebind window controls (splitting and closing)

--- .config/nvim/lua/emilhof/remap.lua
+++ .config/nvim/lua/emilhof/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Window controls
+vim.keymap.set("n", "<leader>|", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>cl", vim.cmd.close)
