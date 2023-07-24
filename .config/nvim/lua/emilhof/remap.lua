85b1be7f3b1d0ac0aed0af9a43bb738502653492
Emil Hofstetter <emilhof@amazon.com>
2023-07-24 13:39:10 -0400

    Add(nvim): Remap keys for window controls

--- .config/nvim/lua/emilhof/remap.lua
+++ .config/nvim/lua/emilhof/remap.lua
@@ -3,4 +3,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Window controls
 vim.keymap.set("n", "<leader>|", vim.cmd.vsplit)
-vim.keymap.set("n", "<leader>cl", vim.cmd.close)
+vim.keymap.set("n", "<leader>:", vim.cmd.split)
+vim.keymap.set("n", "<leader>C", vim.cmd.close)
